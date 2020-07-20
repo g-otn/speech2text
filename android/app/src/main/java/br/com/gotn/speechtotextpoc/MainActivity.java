0de403b31f788f3c296ca552ea3090061eaa6c0d
g-otn <g_otn@hotmail.com>
2020-07-20 15:25:58 -0300

    chore: rename app to Speech2Text and id to br.com.gotn.speechtotextpoc

--- android/app/src/main/java/br/com/gotn/speechtotextpoc/MainActivity.java
+++ android/app/src/main/java/br/com/gotn/speechtotextpoc/MainActivity.java
@@ -1,4 +1,4 @@
-package com.speechtotext;
+package br.com.gotn.speechtotextpoc;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "speechToText";
+    return "Speech2Text";
   }
 }
