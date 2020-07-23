e99f9cb6961351fc0099d7bb0385c3a2d65dc527
g-otn <g_otn@hotmail.com>
2020-07-23 11:09:56 -0300

    refactor: App function into Component, layout/style tweaks, wip audio file reader

--- App.js
+++ App.js
@@ -1,121 +1,213 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
-  TouchableHighlight,
-  Button,
   Switch,
   Linking,
-  useWindowDimensions,
+  Platform,
+  PermissionsAndroid,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Picker } from '@react-native-community/picker';
 const languages = require('./languages.json');
 
-// import { google } from '@google-cloud/speech/build/protos/protos';
-// google.cloud.speech.v1.RecognitionConfig.AudioEncoding.
+import { google } from '@google-cloud/speech/build/protos/protos';
 
-const App = () => {
-  const [languageCode, setLanguageCode] = useState('pt-BR');
-  const [profanityFilter, setProfanityFilter] = useState(false);
-  const [wordTimeOffset, setWordTimeOffset] = useState(false);
-  const [automaticPunctuation, setAutomaticPunctuation] = useState(false);
-  const [model, setModel] = useState('default');
-  const [enhancedModel, setEnhancedModel] = useState(false);
+export default class App extends Component {
 
-  const languagesPickerItems = languages.map(l => (<Picker.Item key={l.bcp47} label={l.name} value={l.bcp47} />));
+  static languagesPickerItems = languages.map(l => (<Picker.Item key={l.bcp47} label={l.name} value={l.bcp47} />));
 
-  return (
-    <>
-      <SafeAreaView style={styles.safeAreaView}>
-        <ScrollView contentContainerStyle={styles.scrollView}>
-          <View>
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      // Metadata
+      encoding: google.cloud.speech.v1p1beta1.RecognitionConfig.AudioEncoding.MP3,
+      sampleRateHertz: 16000,
 
-            <View style={styles.card}>
-              <Text style={styles.heading}>Speech source</Text>
+      // Main Options
+      languageCode: 'pt-BR',
+      profanityFilter: true,
+      wordTimeOffset: false,
+      wordConfidence: false,
+      automaticPunctuation: false,
+      model: 'default',
+      enhancedModel: false,
+    }
+  }
 
-              <View style={styles.sourceView}>
-                <View style={styles.sourceButtonContainer}>
-                  <Icon.Button name='file-audio-o' backgroundColor='#e5ffff' style={styles.sourceButton} color='black' onPress={() => { console.log('a') }}>
-                    <Text style={styles.buttonText}>Select file</Text>
-                  </Icon.Button>
+  // https://github.com/react-native-community/react-native-audio-toolkit/blob/0a2e315d168df1cb02e93d19b98fff5106976882/ExampleApp/src/App.js#L205
+  async requestRecordAudioPermission() {
+    if (Platform.OS !== 'android') {
+      return true;
+    }
+
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+        {
+          title: 'Microphone Permission',
+          message: 'Speech2Text needs access to your microphone to record the speech audio.',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+  }
+
+  test() {
+    console.log(this.state)
+  }
+
+  render() {
+    return (
+      <>
+        <SafeAreaView style={styles.safeAreaView}>
+          <ScrollView contentContainerStyle={styles.scrollView}>
+            <View>
+  
+  
+              <View style={styles.card}>
+                <Text style={styles.heading}>Speech audio</Text>
+  
+                <Text style={{ fontSize: 20 }}>Source</Text>
+                <View style={styles.sourceView}>
+                  <View style={styles.sourceButtonContainer}>
+                    <Icon.Button name='file-audio-o' backgroundColor='#e5ffff' style={styles.sourceButton} color='black' onPress={() => {}}>
+                      <Text style={styles.buttonText}>Select file</Text>
+                    </Icon.Button>
+                  </View>
+                  <View style={styles.sourceButtonContainer}>
+                    <Icon.Button name='microphone' backgroundColor='#e5ffff' color='black' onPress={() => { this.test() }}>
+                      <Text style={styles.buttonText}>Record voice</Text>
+                    </Icon.Button>
+                  </View>
                 </View>
-                <View style={styles.sourceButtonContainer}>
-                  <Icon.Button name='microphone' backgroundColor='#e5ffff' color='black' onPress={() => { console.log('a') }}>
-                    <Text style={styles.buttonText}>Record voice</Text>
-                  </Icon.Button>
+  
+                <View style={styles.sourceView}>
+                  
                 </View>
+  
+                <Text style={styles.sectionTitle}>File metadata</Text>
+                <View style={styles.optionsView}>
+                  <View style={styles.optionContainer}>
+                    <Text style={styles.label} onPress={() => Linking.openURL('https://cloud.google.com/speech-to-text/docs/encoding#audio-encodings')}>
+                      Encoding&nbsp;<Icon style={{ fontSize: 16 }} name="external-link" />
+                    </Text>
+                    {/* google.cloud.speech.v1p1beta1.RecognitionConfig.AudioEncoding
+                        https://cloud.google.com/speech-to-text/docs/encoding#audio-encodings */}
+                    <Picker style={styles.picker} onValueChange={encoding => this.setState({ encoding })} selectedValue={this.state.encoding} mode={"dropdown"}>
+                      <Picker.Item value="MP3" label="MPEG Audio Layer III (MP3)" />
+                      <Picker.Item value="FLAC" label="Free Lossless Audio Codec (FLAC)" />
+                      <Picker.Item value="LINEAR16" label="Linear PCM (16-bit)" />
+                      <Picker.Item value="MULAW" label="μ-law (8-bit PCM encoding)" />
+                      <Picker.Item value="AMR" label="Adaptive Multi-Rate Narrowband (AMR)" />
+                      <Picker.Item value="AMR_WB" label="Adaptive Multi-Rate Wideband (AMR-WB)" />
+                      <Picker.Item value="AMR_WB" label="Adaptive Multi-Rate Wideband (AMR-WB)" />
+                      <Picker.Item value="OGG_OPUS" label="Opus encoded audio frames in an Ogg container" />
+                      <Picker.Item value="SPEEX_WITH_HEADER_BYTE" label="Speex wideband" />
+                    </Picker>
+                  </View>
+                  <View style={styles.optionContainer}>
+                    <Text style={styles.label}>Sample rate</Text>
+                    <Text style={styles.textValue}></Text>
+                    {/* <TextInput style={styles.input} maxLength={5} keyboardType="number-pad" 
+                      onChangeText={value => { setSampleRateHertz(Number(value.replace(/\D/g, ''))); console.log(sampleRateHertz) }} value={sampleRateHertz}/> */}
+                  </View>
+                  <View style={styles.optionContainer}>
+                    <Text style={styles.label}>Bitrate</Text>
+                    <Text style={styles.textValue}></Text>
+                  </View>
+                </View>
+                
               </View>
-            </View>
-
-
-            <View style={styles.card}>
-              <View style={{ alignSelf: 'flex-start' }}>
-                <Text style={styles.heading} onPress={() => Linking.openURL('https://cloud.google.com/speech-to-text/docs/reference/rest/v1/RecognitionConfig')} >
-                  Options&nbsp;<Icon style={{ fontSize: 20 }} name="external-link" />
-                </Text>
+  
+  
+              <View style={styles.card}>
+                <View style={{ alignSelf: 'flex-start' }}>
+                  <Text style={styles.heading} onPress={() => Linking.openURL('https://cloud.google.com/speech-to-text/docs/reference/rest/v1p1beta1/RecognitionConfig')} >
+                    Options&nbsp;<Icon style={{ fontSize: 20 }} name="external-link" />
+                  </Text>
+                </View>
+  
+                <Text style={styles.sectionTitle}>Main options</Text>
+                <View style={styles.optionsView}>
+                  <View style={styles.optionContainer}>
+                    <Text style={styles.label}>Language</Text>
+                    <Picker style={styles.picker} onValueChange={languageCode => this.setState({ languageCode })} selectedValue={this.state.languageCode} mode={"dialog"}>
+                      {App.languagesPickerItems}
+                    </Picker>
+                  </View>
+                  <View style={styles.optionContainer}>
+                    <Text style={styles.label}>Profanity filter</Text>
+                    <Switch style={styles.switch} onValueChange={profanityFilter => this.setState({ profanityFilter })} value={this.state.profanityFilter} />
+                  </View>
+                  <View style={styles.optionContainer}>
+                    <Text style={styles.label}>Word time offset</Text>
+                    <Switch style={styles.switch} onValueChange={wordTimeOffset => this.setState({ wordTimeOffset })} value={this.state.wordTimeOffset} />
+                  </View>
+                  <View style={styles.optionContainer}>
+                    <Text style={styles.label}>Word confidence</Text>
+                    <Switch style={styles.switch} onValueChange={wordConfidence => this.setState({ wordConfidence })} value={this.state.wordConfidence} />
+                  </View>
+                  <View style={styles.optionContainer}>
+                    <Text style={styles.label}>Automatic punctuation</Text>
+                    <Switch style={styles.switch} onValueChange={automaticPunctuation => this.setState({ automaticPunctuation })} value={this.state.automaticPunctuation} />
+                  </View>
+                  <View style={styles.optionContainer}>
+                    <Text style={styles.label}>Model</Text>
+                    <Picker style={styles.picker} onValueChange={model => this.setState=({ model })} selectedValue={this.state.model} mode={"dropdown"}>
+                      <Picker.Item label="Default" value="default" />
+                      <Picker.Item label="Command and search" value="command_and_search" />
+                      <Picker.Item label="Phone call" value="phone_call" />
+                      <Picker.Item label="Video" value="video" />
+                    </Picker>
+                  </View>
+                  <View style={styles.optionContainer}>
+                    <Text style={styles.label}>Enhanced model</Text>
+                    <Switch style={styles.switch} onValueChange={enhancedModel => this.setState({ enhancedModel })} value={this.state.enhancedModel} />
+                  </View>
+                </View>
+  
+                {/* <Text style={styles.sectionTitle}>Recognition metadata</Text>
+                <View style={styles.optionsView}>
+                </View> */}
+  
+                <Icon.Button name='cloud-upload' backgroundColor='#e5ffff' style={styles.sourceButton} color='black' onPress={() => {}} disabled={true}>
+                  <Text style={styles.buttonText}>Send audio</Text>
+                </Icon.Button>
               </View>
-
-              <View style={styles.optionsView}>
-                <View style={styles.optionContainer}>
-                  <Text style={styles.label}>Language</Text>
-                  <Picker style={styles.picker} onValueChange={setLanguageCode} selectedValue={languageCode} mode={"dialog"}>
-                    {languagesPickerItems}
-                  </Picker>
-                </View>
-                <View style={styles.optionContainer}>
-                  <Text style={styles.label}>Profanity filter</Text>
-                  <Switch style={styles.switch} onValueChange={setProfanityFilter} value={profanityFilter} />
-                </View>
-                <View style={styles.optionContainer}>
-                  <Text style={styles.label}>Word time offset</Text>
-                  <Switch style={styles.switch} onValueChange={setWordTimeOffset} value={wordTimeOffset} />
-                </View>
-                <View style={styles.optionContainer}>
-                  <Text style={styles.label}>Automatic punctuation</Text>
-                  <Switch style={styles.switch} onValueChange={setAutomaticPunctuation} value={automaticPunctuation} />
-                </View>
-                <View style={styles.optionContainer}>
-                  <Text style={styles.label}>Model</Text>
-                  <Picker style={styles.picker} onValueChange={setModel} selectedValue={model} mode={"dropdown"}>
-                    <Picker.Item label="Command and search" value="command_and_search" />
-                    <Picker.Item label="Phone call" value="phone_call" />
-                    <Picker.Item label="Video" value="video" />
-                    <Picker.Item label="Default" value="default" />
-                  </Picker>
-                </View>
-                <View style={styles.optionContainer}>
-                  <Text style={styles.label}>Enhanced model</Text>
-                  <Switch style={styles.switch} onValueChange={setEnhancedModel} value={enhancedModel} />
-                </View>
+  
+              <View style={[styles.card, { marginBottom: 20 }]}>
+                <Text style={styles.heading}>Response</Text>
+  
+                <Text style={styles.sectionTitle}>Transcript</Text>
+                <ScrollView style={styles.resultView}>
+                  <Text style={styles.textarea} numberOfLines={1000}>{}</Text>
+                </ScrollView>
+  
+                <Text style={styles.sectionTitle}>Body</Text>
+                <ScrollView style={styles.resultView}>
+                  <Text style={styles.textarea} numberOfLines={1000}>{}</Text>
+                </ScrollView>
               </View>
+  
+  
             </View>
-
-
-            <View style={[styles.card, { marginBottom: 20 }]}>
-              <Text style={styles.heading}>Response</Text>
-
-              <Text style={{ fontSize: 18 }}>Transcript</Text>
-              <ScrollView style={styles.resultView}>
-                <Text style={styles.textarea} numberOfLines={1000}>{}</Text>
-              </ScrollView>
-
-              <Text style={{ fontSize: 18 }}>Body</Text>
-              <ScrollView style={styles.resultView}>
-                <Text style={styles.textarea} numberOfLines={1000}>{}</Text>
-              </ScrollView>
-            </View>
-
-
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+          </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
@@ -128,7 +220,8 @@ const styles = StyleSheet.create({
   },
   card: {
     borderRadius: 15,
-    padding: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
     backgroundColor: '#b2dfdb',
     marginBottom: 10,
   },
@@ -151,10 +244,21 @@ const styles = StyleSheet.create({
     color: 'black',
     fontSize: 18
   },
+  input: {
+    flex: 1.3,
+    backgroundColor: '#e5ffff',
+    color: 'black',
+    maxHeight: 24,
+    borderRadius: 5,
+    paddingVertical: 0,
+    textAlign: 'right'
+  },
+  textValue: {
+    fontSize: 16
+  },
 
   optionsView: {
     flex: 1,
-    padding: 5,
   },
   optionContainer: {
     flex: 1,
@@ -167,12 +271,18 @@ const styles = StyleSheet.create({
   picker: {
     flex: 1.3,
     maxHeight: 24,
+    alignItems: 'flex-end',
+    width: '50%'
   },
   labelIcon: {
     paddingTop: 2,
     fontSize: 15,
     color: 'darkgrey',
     marginRight: 5,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
   label: {
     flex: 1,
@@ -189,5 +299,3 @@ const styles = StyleSheet.create({
   textarea: {
   }
 });
-
-export default App;
