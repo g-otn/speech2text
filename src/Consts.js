8fbe1a2183a5eca9a27f4f93d3e785be57f6e07f
g-otn <g_otn@hotmail.com>
2020-07-24 20:04:27 -0300

    add: backend_url constant placeholder

--- src/Consts.js
+++ src/Consts.js
@@ -1,3 +1,4 @@
 export const Consts = {
-    GOOGLE_STORAGE_BUCKET_URL: 'gs://speech2text-9wivik9b2setrojlvufr',
+    GOOGLE_STORAGE_BUCKET_URL: 'gs://speech2text-9wivik9b2setrojlvufr', // Google Storage Bucket URL to directly upload >=1 min temp audio files
+    BACKEND_URL: 'http://localhost:8050'                                // Backend url to convert files and recieve text
 }
