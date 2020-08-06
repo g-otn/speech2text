ed8790b73ec9ea01ae7bf6072bcda893de1835ec
g-otn <g_otn@hotmail.com>
2020-08-06 19:49:12 -0300

    refactor: Consts names

--- src/Consts.js
+++ src/Consts.js
@@ -1,5 +1,5 @@
 export const Consts = {
-    GOOGLE_CLOUD_API_KEY: '',    // Google Cloud Project API key authorized to use Speech-to-Text API
-    GOOGLE_STORAGE_BUCKET_URL: 'gs://speech2text-9wivik9b2setrojlvufr', // Google Storage Bucket URL to directly upload >=1 min temp audio files
-    BACKEND_URL: 'http://localhost:8050'                                // Backend url to convert files and recieve text
+    GOOGLE_CLOUD_API_KEY: '',            // Google Cloud Project API key authorized to use Speech-to-Text API
+    GOOGLE_STORAGE_BUCKET_NAME: '',      // Google Storage Bucket name to temporarly store >=1 min audio files
+    BACKEND_URL: 'http://localhost:8050' // Backend url to convert files and directly recieve text
 }
